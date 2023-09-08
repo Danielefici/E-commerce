b87508f165ef107ceab861c2c1eb64b765120ff6
Danielefici <danieleficili1@gmail.com>
2023-09-08 12:40:35 +0200

    Add

---
+++ src/Pc_data.js
@@ -0,0 +1,139 @@
+var portatili = [
+  {
+    id: 1,
+    name: "Asus",
+    grado: "B",
+    prezzo: "€350.00",
+    img: "img/Asus.jpg",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  },
+  {
+    id: 2,
+    name: "Dell",
+    grado: "B",
+    prezzo: "€249.00",
+    img: "img/Dell.jpg",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  },
+  {
+    id: 3,
+    name: "Hp",
+    grado: "C",
+    prezzo: "€239.00",
+    img: "img/Hp.jpg",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  },
+  {
+    id: 4,
+    name: "Lenovo",
+    grado: "A",
+    prezzo: "€600.00",
+    img: "img/Lenovo.jpg",
+    promozione: true,
+    sconto: "€200.00",
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  },
+  {
+    id: 5,
+    name: "Mac",
+    grado: "A",
+    prezzo: "€500.00",
+    img: "img/Mac.jpg",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  }
+];
+
+var fissi = [
+  {
+    id: 6,
+    name: "a",
+    grado: "B",
+    prezzo: "€350.00",
+    img: "img/a.webp",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  },
+  {
+    id: 7,
+    name: "b",
+    grado: "B",
+    prezzo: "€350.00",
+    img: "img/b.jpg",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  },
+  {
+    id: 8,
+    name: "c",
+    grado: "B",
+    prezzo: "€350.00",
+    img: "img/c.jpg",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  }
+];
+
+var smartphone = [
+  {
+    id: 9,
+    name: "d",
+    grado: "B",
+    prezzo: "€350.00",
+    img: "img/smar1.jpg",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  },
+  {
+    id: 10,
+    name: "e",
+    grado: "B",
+    prezzo: "€350.00",
+    img: "img/smar2.webp",
+    promozione: false,
+    specifiche: [
+      "Processore: Intel Core i5",
+      "RAM: 8GB",
+      "SSD: 256GB"
+    ]
+  }
+];
